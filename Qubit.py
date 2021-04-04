fe4ff6f504afb22db690599bbdb03c4cc88362f7
OXK719 <81777782+OXK719@users.noreply.github.com>
2021-04-04 16:33:37 +0100

    Update Qubit.py

--- Qubit.py
+++ Qubit.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Tue Apr 28 17:16:32 2020
 
@@ -25,12 +23,14 @@ print(ket_plus, ket_minus)
 #qt.sigmaz * spin , acting on spin yields +1 eigenvalue for vector 'up' and -1 for 'down'
 
 completeness = (plus*ket_plus) + (minus*ket_minus)
-print(completeness) #testing completeness is satisfied
+print('completeness:', completeness) #testing completeness is satisfied
+
+i=complex(0,1)
 
 E1 = 3 # energy of state 1
 E2 = 2 # energy of state 2
 
-prob_plus = 1/3
+prob_plus = 1/3  #initial probability of qubit being in the plus state
 prob_minus = 1 - prob_plus
 
 C_plus = np.sqrt(prob_plus)
@@ -45,44 +45,37 @@ def equation(p):
 x, y =  fsolve(equation, (1, 1))
 
 A_plus = x
-A_mins = y
+A_minus = i*y
 
-modsquare_C_plus = (C_plus)**2
-modsquare_A_plus = (A_plus)**2
 hbar = 1.054571
-
-one = 1
-i = 1
-z=complex(one,i)
 
 t = np.linspace(0, 20, 200)
 
-energy1 = np.exp(-(z-1)*E1*t/hbar)
-energy1star = np.exp((z-1)*E1*t/hbar)
-energy2 = np.exp(-(z-1)*E2*t/hbar)
-energy2star = np.exp((z-1)*E2*t/hbar)
+print(A_plus, A_minus)
+print(C_plus, C_minus)
 
-def Probability_plus(t):
-    return ((modsquare_C_plus*energy1)+(modsquare_A_plus*energy2))*(modsquare_C_plus*energy1star+(modsquare_A_plus*energy2star))
+def Energy(E):
+    return np.exp(-i*E*t/hbar)
 
-plt.figure()
-plt.title("Time evolution of the probability density of a two-state system")
-plt.xlabel("Time (10E-34 s)")
-plt.ylabel("Normalised Probability")
-plt.plot(t, Probability_plus(t))
-plt.savefig('Time_evolution_of_two_state_system.png')
-plt.show()
+def Prob_plus(t):
+    return abs((((C_plus)**2)*Energy(E1))+(((A_plus)**2)*Energy(E2)))**2
 
-# ALternatively one can express the evolution of the probability density as
-
-w = (E1-E2)/hbar
-
-def Probability_plus2(t):
-    return (modsquare_C_plus**2) + (modsquare_A_plus**2) + (2*modsquare_C_plus*modsquare_A_plus*np.cos(w*t))
+def Prob_minus(t):
+    return abs((((C_minus)**2)*Energy(E1))+(((A_minus)**2)*Energy(E2)))**2
 
 plt.figure()
-plt.title("Time evolution of the probability density of a two-state system")
+plt.title("Probability of Qubit measured in plus state")
 plt.xlabel("Time (10E-34 s)")
 plt.ylabel("Normalised Probability")
-plt.plot(t, Probability_plus2(t))
+plt.plot(t, Prob_plus(t))
+plt.savefig('Time_evolution_of_plus_state.png')
 plt.show()
+
+plt.figure()
+plt.title("Probability of Qubit measured in minus state")
+plt.xlabel("Time (10E-34 s)")
+plt.ylabel("Normalised Probability")
+plt.plot(t, Prob_minus(t))
+plt.savefig('Time_evolution_of_minus_state.png')
+plt.show()
+
